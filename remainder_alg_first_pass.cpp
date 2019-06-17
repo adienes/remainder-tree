a425083f48b8e9be097dc49310b995d2d8644176
Andy Dienes <adienes@mit.edu>
2019-06-17 15:44:19 -0400

    int to ZZ

--- remainder_alg_first_pass.cpp
+++ remainder_alg_first_pass.cpp
@@ -3,7 +3,10 @@
 #include <cmath>
 #include <tuple>
 #include <assert.h>
+#include <NTL/ZZ.h>
 
+//using namespace std;
+using namespace NTL;
 
 //to compile this on a mac use
 //c++ -std=c++11 -stdlib=libc++ remainder_alg_first_pass.cpp -o rafp
@@ -15,27 +18,27 @@
 //translates indices in trees to indices in arrays
 //if you want left child of (i,j), do (i+1, 2*j)
 //right child is (i+1, 2*j+1), and parent is (i-1, j//2)
-int flatten_index(int i, int j)
+ZZ flatten_index(ZZ i, ZZ j)
 {
 	assert (j <= pow(2,i)-1);
 	return pow(2,i)+j-1;
 }
 
 //gives tree indices from array index
-std::tuple<int, int> lift_index(int k)
+std::tuple<ZZ, ZZ> lift_index(ZZ k)
 {
-	int i = log2(k+1);
-	int j = k + 1 - pow(2,i);
+	ZZ i = log2(k+1);
+	ZZ j = k + 1 - pow(2,i);
 
 	return std::make_tuple(i,j);
 }
 
 
-template<std::size_t N>
-void print_tree(std::array<int, N> X)
+template<ZZ N>
+void print_tree(std::array<ZZ, N> X)
 {
 	std::cout << std::endl;
-	for (int i = 0; i < N; i++)
+	for (ZZ i = 0; i < N; i++)
 	{
 		std::cout << X[i] << " , ";
 	}
@@ -44,19 +47,19 @@ void print_tree(std::array<int, N> X)
 
 
 //reflects a tree on the y-axis in place
-template<std::size_t N>
-std::array<int, N> reflect_tree(std::array<int, N> X) //why doesn't mutability work the way I think it does?
+template<ZZ N>
+std::array<ZZ, N> reflect_tree(std::array<ZZ, N> X) //why doesn't mutability work the way I think it does?
 {
-	int depth = log2(N);
+	ZZ depth = log2(N);
 
-	for (int i = 1; i <= depth; i++)
+	for (ZZ i = 1; i <= depth; i++)
 	{
-		int width = pow(2,i);
+		ZZ width = pow(2,i);
 
-		for (int j = 0; 2*j < width; j++)
+		for (ZZ j = 0; 2*j < width; j++)
 		{
-			int a = flatten_index(i,j);
-			int b = flatten_index(i, width-j-1);
+			ZZ a = flatten_index(i,j);
+			ZZ b = flatten_index(i, width-j-1);
 
 			//this switches the two locations
 			X[b] = X[a]^X[b];
@@ -71,18 +74,18 @@ std::array<int, N> reflect_tree(std::array<int, N> X) //why doesn't mutability w
 //given an array of size N, returns the product tree of size 2N
 //assumes N is a power of 2
 //it won't have to be (we can pad it etc.) but this is just a preliminary version
-template<std::size_t N>
-std::array<int, 2*N-1> product_tree(std::array<int, N> X, std::array<int, 2*N-1> mtree = {})
+template<ZZ N>
+std::array<ZZ, 2*N-1> product_tree(std::array<ZZ, N> X, std::array<ZZ, 2*N-1> mtree = {})
 {
-	//INPUT: a list of integers; OUTPUT: a product tree of double the size
-	int depth = log2(N); //round this UP when not power of 2
+	//INPUT: a list of ZZegers; OUTPUT: a product tree of double the size
+	ZZ depth = log2(N); //round this UP when not power of 2
 
-	std::array<int, 2*N-1> ptree;
+	std::array<ZZ, 2*N-1> ptree;
 
 	//initialize leaves
-	for (int j = 0; j < pow(2, depth); j++)
+	for (ZZ j = 0; j < pow(2, depth); j++)
 	{
-		int leaf = flatten_index(depth,j);
+		ZZ leaf = flatten_index(depth,j);
 
 		ptree[leaf] = X[j];
 
@@ -93,13 +96,13 @@ std::array<int, 2*N-1> product_tree(std::array<int, N> X, std::array<int, 2*N-1>
 	}
 
 	//build up the product tree recursively
-	for (int i = depth-1; i >= 0; i--)
+	for (ZZ i = depth-1; i >= 0; i--)
 	{
-		for (int j = 0; j < pow(2,i); j++)
+		for (ZZ j = 0; j < pow(2,i); j++)
 		{
-			int parent = flatten_index(i,j);
-			int left = 2*parent + 1;
-			int right = left + 1;
+			ZZ parent = flatten_index(i,j);
+			ZZ left = 2*parent + 1;
+			ZZ right = left + 1;
 
 			//padding is not necessary if on this step we catch IndexError with 1
 			ptree[parent] = ptree[left]*ptree[right];
@@ -116,24 +119,24 @@ std::array<int, 2*N-1> product_tree(std::array<int, N> X, std::array<int, 2*N-1>
 }
 
 
-template<std::size_t n>
-std::array<int, n> accumulating_tree(std::array<int, n> X, std::array<int, n> mtree = {})
+template<ZZ n>
+std::array<ZZ, n> accumulating_tree(std::array<ZZ, n> X, std::array<ZZ, n> mtree = {})
 {
 	//code cut from body of accumulating_remainder_tree
 	//this can be used to store the product tree of A modulo the mi
 	//this can be applied to any tree; for our applications we will only apply it to product trees
 	//INPUT: a tree; OUTPUT: accumulating tree (of same size)
-	int depth = log2(n);
+	ZZ depth = log2(n);
 
-	std::array<int, n> acctree = {1};
+	std::array<ZZ, n> acctree = {1};
 
-	for (int i = 0; i < depth; i++)
+	for (ZZ i = 0; i < depth; i++)
 	{
-		for (int j = 0; j < pow(2, i); j++)
+		for (ZZ j = 0; j < pow(2, i); j++)
 		{
-			int parent = flatten_index(i,j);
-			int left = 2*parent + 1;
-			int right = left+1;
+			ZZ parent = flatten_index(i,j);
+			ZZ left = 2*parent + 1;
+			ZZ right = left+1;
 
 			acctree[left] = acctree[parent];
 			acctree[right] = acctree[parent]*X[left];
@@ -159,29 +162,29 @@ std::array<int, n> accumulating_tree(std::array<int, n> X, std::array<int, n> mt
 //so might make sense to pad beginning of m with a 1, and pad end of A with a 1
 //given A0, A1, ... An, and m0, m1, ... mn, this returns
 //1, A0 mod m1, A0*A1 mod m2, ... A0*...An-1 mod mn
-template<std::size_t n>
-std::array<int, n> accumulating_remainder_tree(std::array<int, n> A, std::array<int, n> m)
+template<ZZ n>
+std::array<ZZ, n> accumulating_remainder_tree(std::array<ZZ, n> A, std::array<ZZ, n> m)
 {
-	int depth = log2(n);
-	std::array<int, 2*n-1> m_ptree = product_tree(m);
+	ZZ depth = log2(n);
+	std::array<ZZ, 2*n-1> m_ptree = product_tree(m);
 
-	std::array<int, 2*n-1> m_acctree = reflect_tree(accumulating_tree(reflect_tree(m_ptree)));
+	std::array<ZZ, 2*n-1> m_acctree = reflect_tree(accumulating_tree(reflect_tree(m_ptree)));
 
 
 	//this power tree is reduced modulo the reflected m_ftree
-	std::array<int, 2*n-1> A_ptree = product_tree(A, m_acctree);
+	std::array<ZZ, 2*n-1> A_ptree = product_tree(A, m_acctree);
 	//delete m_acctree;
 
 	print_tree(m_ptree);
 	print_tree(m_acctree);
 	print_tree(A_ptree);
 
-	std::array<int, 2*n-1> A_rtree = accumulating_tree(A_ptree, m_ptree);
+	std::array<ZZ, 2*n-1> A_rtree = accumulating_tree(A_ptree, m_ptree);
 
 	//need a nice array slice here...
-	std::array<int, n> C;
+	std::array<ZZ, n> C;
 
-	for (int j = 0; j < pow(2, depth); j++)
+	for (ZZ j = 0; j < pow(2, depth); j++)
 	{
 		C[j] = A_rtree[flatten_index(depth, j)];
 	}
@@ -196,15 +199,10 @@ std::array<int, n> accumulating_remainder_tree(std::array<int, n> A, std::array<
 
 int main()
 {
-	std::array<int, 8> A = {1,2,3,4,5,6,7,1};
-	std::array<int, 8> m = {1,2,3,1,5,1,7,1};
+	std::array<ZZ, 8> A = {1,2,3,4,5,6,7,1};
+	std::array<ZZ, 8> m = {1,2,3,1,5,1,7,1};
 
-	std::array<int, 8> remainders = accumulating_remainder_tree(A,m);
+	std::array<ZZ, 8> remainders = accumulating_remainder_tree(A,m);
 
-	for (int i = 0; i < remainders.size(); i++)
-	{
-		std::cout << remainders[i] << " , ";
-	}
-	std::cout << std::endl;
-	return 0;
+	print_tree(remainders);
 }
