5ede3beecd3110f0a18b3e315593493a2b84f400
Daniel Liu <hi.daniel.liu@gmail.com>
2019-06-19 16:46:57 -0400

    filled in constructor for rem_tree

--- rem_tree/rem_tree.cpp
+++ rem_tree/rem_tree.cpp
@@ -1,9 +1,16 @@
 #include "rem_tree.h"
 
+using namespace std;
+using namespace NTL;
 
-rem_tree::rem_tree(){
-
+rem_tree::rem_tree(ZZ startBound, ZZ endBound, ZZ (*const startVal)(ZZ), Vec<ZZ> A, Vec<ZZ> m){
+	this->startBound = startBound;
+	this->endBound = endBound;
+	this->startVal = startVal; 
+	this->A = A;
+	this->m = m;
 }
 rem_tree::~rem_tree(){
 
 }
+
--- rem_tree/rem_tree.h
+++ rem_tree/rem_tree.h
@@ -1,17 +1,20 @@
 #ifndef REM_TREE_H
 #define REM_TREE_H
 
+#include <NTL/ZZ.h>
+#include <NTL/vector.h>
+
 
 class rem_tree{
 private:
-	int startBound;		// lower bound in interval
-	int endBound; 		// upper bound in interval
-	int startValue;		// starting value inputted into moduli
-	int A;				// Array of values in leaves
-	int m; 				// Array of moduli in leaves
+	ZZ startBound;				// lower bound in interval
+	ZZ endBound; 				// upper bound in interval
+	ZZ (*const startVal)(ZZ);	// pointer to function that calculates start value mod product of moduli (IS THIS RIGHT???)
+	Vec<ZZ> A;						// Array of values in leaves
+	Vec<ZZ> m; 						// Array of moduli in leaves
 
 public:
-	rem_tree();
+	rem_tree(ZZ startBound, ZZ endBound, ZZ (*const startVal)(ZZ), Vec<ZZ> A, Vec<ZZ> m);
 	~rem_tree();
 
 };
